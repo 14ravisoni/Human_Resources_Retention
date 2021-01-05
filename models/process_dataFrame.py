646dc8d05327bc92251d16a1ff5031c6336d8546
14ravisoni <14ravisoni@gmail.com>
2021-01-05 19:03:52 +0530

    printing raw and proceesed data test1

--- models/process_dataFrame.py
+++ models/process_dataFrame.py
@@ -1,11 +1,11 @@
 def process_dataframe(main_df):
     import pandas as pd
-    from sklearn.preprocessing import StandardScaler
+    # from sklearn.preprocessing import StandardScaler
 
     main_df.fillna(main_df.mean(), inplace=True)
     main_df.drop(columns='employee_id', inplace=True)
     main_df = pd.get_dummies(main_df, columns=['department', 'salary'], drop_first=True)
 
-    main_df = StandardScaler().fit_transform(main_df)
+    # main_df = StandardScaler().fit_transform(main_df)
     
     return main_df
