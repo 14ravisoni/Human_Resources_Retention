82d771dae62684e8def0b08acb3c20b17b79c1a0
14ravisoni <14ravisoni@gmail.com>
2021-01-05 17:13:12 +0530

    some changes

--- app.py
+++ app.py
@@ -8,5 +8,10 @@ def home():
     return render_template('index.html')
 
 
+@app.route('/result', methods=['POST'])
+def result():
+    return 'Good click but no'
+
+
 if __name__ == '__main__':
     app.run(debug=True)
--- models/process_dataFrame.py
+++ models/process_dataFrame.py
@@ -1,9 +1,11 @@
-def process_dataFrame(main_df):
+def process_dataframe(main_df):
+    import pandas as pd
+    from sklearn.preprocessing import StandardScaler
+
     main_df.fillna(main_df.mean(), inplace=True)
     main_df.drop(columns='employee_id', inplace=True)
     main_df = pd.get_dummies(main_df, columns=['department', 'salary'], drop_first=True)
-    
-    from sklearn.preprocessing import StandardScaler
+
     main_df = StandardScaler().fit_transform(main_df)
     
     return main_df
